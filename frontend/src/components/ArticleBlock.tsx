b6378951fc9a754de4e50410536720a1119fc7b7
itsevelync <evelynchen555@gmail.com>
2025-02-11 16:51:15 -0500

    Slight ArticleBlock edits to match Figma

--- frontend/src/components/ArticleBlock.tsx
+++ frontend/src/components/ArticleBlock.tsx
@@ -7,12 +7,12 @@ function ArticleBlock({ post, height }: ArticleBlockProps) {
         <div className='cursor-pointer rounded-md bg-cover bg-center h-[200px] w-full flex items-end p-3'
         onClick={()=>navigate('news/'+post.id)}
         style={{
-            backgroundImage: `linear-gradient(to bottom, rgba(26, 30, 71, 0.15), rgba(26, 30, 71, 1) 95%), url(${post.coverImage})`,
+            backgroundImage: `linear-gradient(to bottom, rgba(26, 30, 71, 0.15), rgba(26, 30, 71, 1) 75%), url(${post.coverImage})`,
             height: `${height}`
         }}>
             <div>
                 <h3 className="text-white font-bold text-2xl/7 mb-2">{post.title}</h3>
-                <h6 className="text-[#BAC0FF] uppercase text-sm">{post.category} &#8226; {post.author}</h6>
+                <h6 className="text-[#BAC0FF] text-sm"><span className="uppercase">{post.category}</span> &#8226; {post.author}</h6>
             </div>
         </div>
     )
