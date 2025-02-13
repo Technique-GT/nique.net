f4337d0cc86c48315b8f8400977a7d3eda86a0fb
dmueller34 <dmueller34@gatech.edu>
2025-02-13 13:41:59 -0500

    added a back button in the article page

--- frontend/src/pages/Article.tsx
+++ frontend/src/pages/Article.tsx
@@ -1,14 +1,17 @@
 import { useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 
 export default function Article() {
         const {id}=useParams();
+        const navigate = useNavigate()
 
         useEffect(()=>{
             console.log(id)
         },[])
+
     return (
         <>
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" onClick={() => navigate('/')}> Go back</button>
             <h1 className="text-3xl font-bold">
                 This is an article! Article id: {id}. Someone set up an actual db lol.
             </h1>
