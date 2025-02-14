6ea367ce58e220b2f2c2bc0aec13685f055f4e1e
EthanLi15 <ethanli1360@gmail.com>
2025-02-14 12:56:19 -0500

    Auth

--- backend/server.ts
+++ backend/server.ts
@@ -1,13 +1,19 @@
 import express from "express";
 import cors from "cors";
+import auth from "./routes/auth.ts";
 import records from "./routes/record.ts";
+
+import crypto from 'node:crypto';
 
 const PORT = process.env.PORT || 5050;
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use("/auth", auth);
 app.use("/record", records);
+
+console.log(crypto.randomBytes(64).toString('hex'));
 
 // start the Express server
 app.listen(PORT, () => {
--- frontend/src/pages/AdminPage.tsx
+++ frontend/src/pages/AdminPage.tsx
@@ -1,9 +1,78 @@
+import axios from "axios";
+import cookie from "js-cookie";
+import { useEffect, useState } from "react";
+
 export default function AdminPage() {
-    return (
-      <div>
-        <title>Admin Page</title>
-        <h1>Admin Page</h1>
-        <p>Defualt Admin page for now</p>
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    const jwt_token = cookie.get("jwt");
+    if (cookie.get("jwt") === undefined) {
+      setMessage("");
+    } else {
+      axios.get("http://127.0.0.1:5050/auth/test_token", {
+        headers: { Authorization: `Bearer ${jwt_token}` }
+      }).then(() => {
+        setMessage("Welcome back!");
+      }, () => {
+        setMessage("Login session expired, please log in again");
+      })
+    }
+  }, []);
+
+  return (
+    <div className="">
+      <title>Admin Page</title>
+      <h1>Admin Page</h1>
+      <p>Defualt Admin page for now</p>
+      <div className="w-80 flex flex-col border-1 rounded p-4 bg-gray-300 justify-center">
+        <p>Username</p>
+        <input
+          className="border-1 rounded border-gray-500 text-2xl h-10 bg-white px-1 w-full"
+          type="text"
+          id="username"
+          name="username"
+          value={username}
+          onChange={(e) => {
+            setUsername(e.target.value);
+          }}
+        />
+        <p className="mt-2">Password</p>
+        <input
+          className="border-1 rounded border-gray-500 text-2xl h-10 bg-white px-1 w-full"
+          type="password"
+          id="password"
+          name="password"
+          value={password}
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+        />
+        <button
+          className="self-end rounded bg-sky-700 hover:bg-sky-800 hover:cursor-pointer text-white px-4 py-1 mt-2"
+          onClick={() => {
+            axios
+              .post("http://127.0.0.1:5050/auth/login", {
+                username: username,
+                password: password,
+              })
+              .then(
+                (res) => {
+                  alert(res.data);
+                  cookie.set("jwt", res.data);
+                },
+                (reason) => {
+                  alert(reason);
+                }
+              );
+          }}
+        >
+          Log in
+        </button>
+        <p>{message}</p>
       </div>
-    );
+    </div>
+  );
 }
