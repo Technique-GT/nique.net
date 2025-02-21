6b193fda20a735a0a89fa975cd2d4f31d3686e8e
Ethan Loo Yu Teng <eloo6@gatech.edu>
2025-02-21 14:00:42 -0500

    Polished carousel

--- frontend/src/components/Carousel.tsx
+++ frontend/src/components/Carousel.tsx
@@ -12,16 +12,17 @@ export default function Carousel( {posts}: ArticleListProps ) {
   const navigate=useNavigate();
   
   return (
-    <section className="pt-[7rem] pb-[2rem]">
       <div className="lg:mx-auto max-w-5xl mx-[1.5rem]">
         <Swiper
           modules={[EffectCoverflow, Pagination, Navigation]}
+          navigation={true}
           effect={'coverflow'}
           loop={true}
-          spaceBetween={30}
+          spaceBetween={50}
           slidesPerView={1}
           pagination={{
             clickable: true,
+            dynamicBullets: true,
           }}
           centeredSlides={true}
           grabCursor={true}
@@ -33,13 +34,19 @@ export default function Carousel( {posts}: ArticleListProps ) {
         >
           {posts.map((p, index) => {
             return (
-              <SwiperSlide key={index} onClick={()=>navigate('news/'+p.id)} className='cursor-pointer'>
+              <SwiperSlide 
+                key={index} 
+                onClick={()=>navigate('news/'+p.id)} 
+                style={{
+                  backgroundImage: `linear-gradient(to bottom, rgba(26, 30, 71, 0.15), rgba(26, 30, 71, 1) 75%), url(${p.coverImage})`,
+                }}
+                className='cursor-pointer rounded-lg'
+              >
                 <img src={p.coverImage} alt="" className='rounded-lg'/>
               </SwiperSlide>
             );
           })}
         </Swiper>
       </div>
-    </section>
   );
 }
