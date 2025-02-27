012f31635122e2a758fc76361fcc12cdb6b9269d
Ethan Loo Yu Teng <eloo6@gatech.edu>
2025-02-27 15:59:44 -0500

    Add News page and integrate into routing
    waiting for PR to integrate other changes from that

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,6 +5,7 @@ import Navbar from "./components/Navbar";
 import AdminPage from './pages/AdminPage';
 import Dashboard from './pages/Dashboard';
 import Sports from "./pages/Sports";
+import News from "./pages/News";
 
 function App() {
 
@@ -19,6 +20,7 @@ function App() {
             <Route path="/admin" element={<AdminPage />} />
             <Route path="/dashboard" element={<Dashboard />} />
             <Route path="/sports" element={<Sports />} />
+            <Route path="/news" element={<News />} />
           </Routes>
         </div>
       </div>
