412f81bb3a7768db9ec0492f12c8bd2c5f6d1de4
EthanLi15 <ethanli1360@gmail.com>
2025-03-09 00:33:33 -0500

    Threw on navbar

--- frontend/src/pages/SearchPage.tsx
+++ frontend/src/pages/SearchPage.tsx
@@ -1,45 +1,55 @@
-import { useEffect, useState } from 'react'
-import { FaSearch, FaTimes } from 'react-icons/fa'
-import { useLocation, useNavigate } from 'react-router-dom'
+import { useEffect, useState } from "react";
+import { FaSearch, FaTimes } from "react-icons/fa";
+import { useLocation, useNavigate } from "react-router-dom";
+import Navbar from "../components/Navbar";
 
 const SearchPage = () => {
+  const [text, setText] = useState("");
+  const [searchedText, setSearchedText] = useState("");
+  const navigate = useNavigate();
 
-    const [text, setText] = useState('')
-    const [searchedText, setSearchedText] = useState('')
-    const navigate = useNavigate()
-
-    const handleSearch = () => {
-        if (text.trim()) {
-            navigate(`/search?query=${encodeURIComponent(text)}`)
-            setText("")
-        }
+  const handleSearch = () => {
+    if (text.trim()) {
+      navigate(`/search?query=${encodeURIComponent(text)}`);
+      setText("");
     }
+  };
 
-    const location = useLocation()
-    const queryParams = new URLSearchParams(location.search)
-    const queryText = queryParams.get("query") || "No search term" 
+  const location = useLocation();
+  const queryParams = new URLSearchParams(location.search);
+  const queryText = queryParams.get("query") || "No search term";
 
-    useEffect(() => {
-        setSearchedText(queryText)
-    }, [queryText])
+  useEffect(() => {
+    setSearchedText(queryText);
+  }, [queryText]);
 
   return (
-    <div className="max-w-7xl mx-auto p-6">
+    <>
+      <Navbar />
+      <div className="max-w-7xl mx-auto p-6">
         <div className="flex items-center justify-between border border-gray-300 rounded-full p-2 bg-white">
-            <input
+          <input
             type="text"
             value={text}
             className="flex-grow mx-2 outline-none bg-transparent"
             onChange={(e) => setText(e.target.value)}
             placeholder="Search Article"
+          />
+          <button>
+            <FaSearch
+              onClick={handleSearch}
+              className="text-blue-950 hover:text-blue-900"
+              size="20"
             />
-            <button><FaSearch onClick={handleSearch} className="text-blue-950 hover:text-blue-900" size='20'/></button>
+          </button>
         </div>
-        <div className='font-bold py-3 text-2xl'>
-            Searching results for <span className="text-blue-900">{searchedText}</span>
+        <div className="font-bold py-3 text-2xl">
+          Searching results for{" "}
+          <span className="text-blue-900">{searchedText}</span>
         </div>
-    </div>
-  )
-}
+      </div>
+    </>
+  );
+};
 
-export default SearchPage
+export default SearchPage;
