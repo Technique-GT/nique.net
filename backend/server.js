607e72543c1314f3574795ef96d8d5c47d78bd9f
Ruben Taylor <rubentaylor70@gmail.com>
2025-04-11 00:18:58 -0400

    update articles, tags, categories, and server

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,9 @@ const dotenv = require('dotenv');  // Import dotenv to load environment variable
 const cors = require('cors');
 const authRoutes = require('./routes/auth.routes');
 const staffRoutes = require('./routes/staff.routes');
+const categoryRoutes = require('./routes/category.routes');
+const tagRoutes = require('./routes/tag.routes');
+const articleRoutes = require('./routes/article.routes');
 
 // Load environment variables from config.env
 dotenv.config({ path: './config.env' });  // Specify the correct path to your config.env file
@@ -22,7 +25,7 @@ app.use(cors({
 }));
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI, {
+mongoose.connect(process.env.ATLAS_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
@@ -34,6 +37,15 @@ app.use('/api/auth', authRoutes);  // Prefix routes with /api/auth
 
 // Use staff routes
 app.use('/api/staff', staffRoutes);  // Prefix routes with /api/staff
+
+// Use  category routes
+app.use('/api/categories', categoryRoutes); 
+
+// User tags route
+app.use('/api/tags', tagRoutes);
+
+//article route
+app.use('/api/articles', articleRoutes);
 
 // Example protected route
 app.get('/api', (req, res) => {
