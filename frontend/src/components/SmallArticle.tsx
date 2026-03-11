e3122ef555797d15ec08ca039acdbb0893ad09ed
Ethan Loo Yu Teng <eloo6@gatech.edu>
2026-03-10 20:07:06 -0400

    adjust opinions layout -- remove column in main section

--- frontend/src/components/SmallArticle.tsx
+++ frontend/src/components/SmallArticle.tsx
@@ -10,7 +10,7 @@ function SmallArticle({ articles, direction }: SmallArticleProps) {
     const navigate = useNavigate();
     return (
         <div>
-            {articles.slice(0, 10).map((article, index) => {
+            {articles.map((article, index) => {
                 const link = getArticleLink(article);
                 const image = getArticleImage(article);
                 const author = getArticleAuthorName(article);
