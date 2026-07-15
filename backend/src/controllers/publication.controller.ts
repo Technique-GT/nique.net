2fd184de5029f2e3080cf3d53146175451f7630f
NhaHanNguyen <nhahannguyen14@gmail.com>
2026-07-14 20:39:13 -0400

    added database schema (backend) and API routes for print publications

---
+++ backend/src/controllers/publication.controller.ts
@@ -0,0 +1,36 @@
+//
+//  printPublicationController.ts
+//  
+//
+//  Created by Nha Han Nguyen on 7/1/26.
+//
+
+import { Request, Response } from 'express';
+import PrintPublication, { IPrintPublication } from '../models/printpublication';
+
+// get dates
+export const getPublications = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const publications: IPrintPublication[] = await PrintPublication.find().sort({ publishDate: -1 });
+    res.status(200).json(publications);
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to fetch publication dates.' });
+  }
+};
+
+// enter new date
+export const createPublication = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const { issueName, issueType, publishDate } = req.body;
+    const newPublication: IPrintPublication = new PrintPublication({
+      issueName,
+      issueType,
+      publishDate
+    });
+    
+    await newPublication.save();
+    res.status(201).json(newPublication);
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to create publication date.' });
+  }
+};
